d577c7063a506717626919128c1dd9ab445ed0a9
lydia205 <lydiaomombo35@gmail.com>
2026-04-17 02:11:05 -0700

    created variable to hold song

--- MusicApp/app/src/main/java/com/example/musicapp/Song.kt
+++ MusicApp/app/src/main/java/com/example/musicapp/Song.kt
@@ -3,5 +3,9 @@ package com.example.musicapp
 import android.provider.MediaStore
 import java.time.Year
 
-class Song (songname: String, songArtists: String, songYear: Int,songDuration) {
+class Song (songname: String, songArtists: String, songYear: Int,songDuration: String) {
+    //created variable to hold song object's attributes
+    var name: String = ""
+    var artists: String = ""
+    var Year
 }
