94d02bb8dbc18745a8b4fc0fd6b8c814971c07e7
lydia205 <lydiaomombo35@gmail.com>
2026-04-17 02:03:32 -0700

    added the song class

--- MusicApp/app/src/main/java/com/example/musicapp/MainActivity.kt
+++ MusicApp/app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -11,6 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //accessing the Song class to populate song objects with specific attributes
+        var  song1 = Song("Choosi'Texas", "Ell")
+        var  song2
+        var  song3
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- MusicApp/app/src/main/java/com/example/musicapp/Song.kt
+++ MusicApp/app/src/main/java/com/example/musicapp/Song.kt
@@ -3,5 +3,5 @@ package com.example.musicapp
 import android.provider.MediaStore
 import java.time.Year
 
-class Song (songname: String, songArtists: String, songYear: ) {
+class Song (songname: String, songArtists: String, songYear: Int,songDuration) {
 }
