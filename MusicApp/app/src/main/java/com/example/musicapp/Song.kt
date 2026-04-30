4f946cf0a997c4a402e8205cf9f06b8488904e7f
lydia205 <lydiaomombo35@gmail.com>
2026-04-30 01:40:22 -0700

    added variable artist

--- MusicApp/app/src/main/java/com/example/musicapp/Song.kt
+++ MusicApp/app/src/main/java/com/example/musicapp/Song.kt
@@ -2,11 +2,13 @@ package com.example.musicapp
 
 import android.provider.MediaStore
 import android.util.Log
+import android.view.Display
 import java.time.Year
 import kotlin.time.Duration
 
-class Song (SongName: String, songArtist: String, songYear: Int,songDuration: String){
+class Song ( var songName: String, var songArtist: String,  var songYear: Int, var songDuration: String){
     //created variable to hold song object's attributes
+
     var name: String = ""
     var Artist: String = ""
     var Year: Int = 0
@@ -15,14 +17,31 @@ class Song (SongName: String, songArtist: String, songYear: Int,songDuration: St
     constructor(songName: String, songArtists: String) : this(songName, songArtists, 0,"")
     }
 
-    //initialisAing variables
+    //secondary constructor
+    con
+
+
+    //initialize variables
     init {
         name  = songName
         Artist = songArtist
-        year = songYear
+        Year = songYear
         Duration = songDuration
     }
 
 fun play () {
     Log.v("song", "$name" playing now")
 }
+fun Pause (){
+    Log.v( "song", "$name paused now")
+}
+fun Stop (){
+    Log.v("song", "$name stopped now")
+}
+fun Favourite(): Boolean {
+    Log.v("song", "$name favourite now")
+    return true
+}
+fun DisplaySong(): String
+      val artist = ""
+       return "$name - $artist ($duration)"}
